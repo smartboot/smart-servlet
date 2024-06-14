ebd0414903a0d1d105ff81564d8dd74b104437b9
zhengjw22 <zhengjunweimail@163.com>
2024-06-14 22:21:53 +0800

    feat(springboot-demo): 添加session管理和注销功能
    新增了两个GET请求处理方法，用于演示session的添加和注销功能。
    在添加功能中，通过设置session属性来演示session的使用。在注销功能中，
    通过多次调用invalidate方法来展示session的失效过程。

--- springboot-demo/src/main/java/org/smartboot/demo/starter/Bootstrap.java
+++ springboot-demo/src/main/java/org/smartboot/demo/starter/Bootstrap.java
@@ -12,22 +12,45 @@ package org.smartboot.demo.starter;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.SessionAttribute;
 
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
 import java.security.Principal;
+import java.util.Date;
 
 @RestController
 @SpringBootApplication
 public class Bootstrap {
+    @GetMapping(path = "/add")
+    void add(HttpServletRequest request, HttpServletResponse response) {
+        HttpSession session = request.getSession();
+        session.setAttribute("time", new Date().getTime());
+        System.out.println("session...");
+    }
+    @GetMapping(path = "/logout")
+    void logout(HttpServletRequest request, HttpServletResponse response) {
+        HttpSession session = request.getSession();
+        session.invalidate();
+        session = request.getSession();
+        session.invalidate();
+        session = request.getSession();
+        session.invalidate();
+        request.getSession();
+        System.out.println("invalidate...");
+    }
 
     @RequestMapping("/index")
-    String home(HttpServletRequest request) throws ServletException {
+    String home(HttpServletRequest request, @SessionAttribute(name = "time", required = false) String time) throws ServletException {
         Principal principal = request.getUserPrincipal();
         System.out.println(principal);
-        return "Hello World!";
+        System.out.println(request.getSession().getAttribute("time"));
+        return "Hello World!" + time;
     }
 
     public static void main(String[] args) {
