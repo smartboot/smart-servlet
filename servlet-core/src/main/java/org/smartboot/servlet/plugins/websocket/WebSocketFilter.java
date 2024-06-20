1297590fd6fa30e0ec9a5978620b53d6cb783422
zhengjw22 <zhengjunweimail@163.com>
2024-06-20 19:14:52 +0800

    feat(servlet-core): update smart-http-server dependency and related config
    Upgrade the smart-http-server dependency from version 1.4.3 to 1.5.1. Adjust
    the corresponding group ID to 'io.github.smartboot.http'. Ensure compatibility
    with new version by refactoring WebSocketFilter to include init method and
    handling any breaking changes. Also, remove unnecessary comments and systemdependency configurations from springboot-demo project to clean up the code.

--- servlet-core/src/main/java/org/smartboot/servlet/plugins/websocket/WebSocketFilter.java
+++ servlet-core/src/main/java/org/smartboot/servlet/plugins/websocket/WebSocketFilter.java
@@ -9,6 +9,7 @@ import org.smartboot.servlet.plugins.websocket.impl.WebSocketServerContainerImpl
 
 import javax.servlet.Filter;
 import javax.servlet.FilterChain;
+import javax.servlet.FilterConfig;
 import javax.servlet.ServletException;
 import javax.servlet.ServletRequest;
 import javax.servlet.ServletResponse;
@@ -21,6 +22,10 @@ import java.util.List;
 import java.util.Map;
 
 public class WebSocketFilter implements Filter {
+    @Override
+    public void init(FilterConfig filterConfig) throws ServletException {
+        //nothing
+    }
 
     @Override
     public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain) throws IOException, ServletException {
