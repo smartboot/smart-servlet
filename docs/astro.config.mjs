3409e14d8b0b05f81a890fdfb534730a0de5d164
zhengjw22 <zhengjunweimail@163.com>
2024-07-24 18:54:32 +0800

    docs

---
+++ docs/astro.config.mjs
@@ -0,0 +1,27 @@
+import { defineConfig } from 'astro/config';
+import starlight from '@astrojs/starlight';
+
+// https://astro.build/config
+export default defineConfig({
+	integrations: [
+		starlight({
+			title: 'smart-servlet',
+			social: {
+				github: 'https://github.com/smartboot/smart-servlet',
+			},
+			sidebar: [
+				{
+					label: 'Guides',
+					items: [
+						// Each item here is one entry in the navigation menu.
+						{ label: 'Example Guide', slug: 'guides/example' },
+					],
+				},
+				{
+					label: 'Reference',
+					autogenerate: { directory: 'reference' },
+				},
+			],
+		}),
+	],
+});
